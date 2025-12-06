0f4fa8495b22f5d8ecb15c7f2c0267468de06457
alienx5499 <p2005p5499p@gmail.com>
2025-12-06 22:00:00 +0530

    Add test files: Basic test structure

---
+++ rideshare-backend/src/test/java/org/example/AppTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppTest {
+    @Test
+    public void contextLoads() {
+        // Test that the application context loads successfully
+    }
+}
